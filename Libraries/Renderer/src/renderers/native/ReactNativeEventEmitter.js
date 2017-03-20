8dc4275da2f8cd51d6968106a90855e1c36c4d32
Valentin Shergin <shergin@fb.com>
2017-03-19 21:43:14 -0700

    Removed harmful optimization in ReactNativeEventEmitter
    Reviewed By: spicyj
    
    Differential Revision: D4729779
    
    fbshipit-source-id: 2dd5ec10d42df7f24804796c4100eca107edeedb

--- Libraries/Renderer/src/renderers/native/ReactNativeEventEmitter.js
+++ Libraries/Renderer/src/renderers/native/ReactNativeEventEmitter.js
@@ -118,11 +118,6 @@ var ReactNativeEventEmitter = {
   ) {
     var nativeEvent = nativeEventParam || EMPTY_NATIVE_EVENT;
     var inst = ReactNativeComponentTree.getInstanceFromNode(rootNodeID);
-    if (!inst) {
-      // If the original instance is already gone, we don't have to dispatch
-      // any events.
-      return;
-    }
     ReactUpdates.batchedUpdates(function() {
       ReactNativeEventEmitter.handleTopLevel(
         topLevelType,
